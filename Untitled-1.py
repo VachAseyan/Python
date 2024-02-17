312b21e62558e29732eeb7740746d4552dc28443
VachAseyan <159058094+VachAseyan@users.noreply.github.com>
2024-02-17 22:15:16 +0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,62 @@
+print ("Hello World")
+
+name = input("Enter your name: ")
+
+print("Hello, " + name + "!")
+
+number1=float(input("Greq arajin tiv@"))
+
+number2=float(input("Greq erkrord tiv@"))
+
+print(number1+number2)
+print(number1/number2)
+print(number1%number2)
+print(number1*number2)
+
+
+
+age = 25
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+
+
+
+
+# Lists
+my_list = [1, 2, 3, 4, 5]
+for item in my_list:
+    print(item)
+# Dicts
+my_dict = {'name': 'Alice', 'age': 30}
+for key, value in my_dict.items():
+    print(key, value)
+# Sets
+my_set = {1, 2, 3, 4, 5}
+for item in my_set:
+    print(item)
+
+import math
+
+number1=float(input("first number"))
+number2=float(input("second number"))
+
+gumarum=number1+number2
+hanum=number1-number2
+bazmapatkum=number1*number2
+bajanum=number1/number2
+mnacord=number1%number2
+armat1=math.sqrt(number1)
+armat2=math.sqrt(number2)
+
+
+print(gumarum)
+print(hanum)
+print(bazmapatkum)
+print(bajanum)
+print(mnacord)
+print(armat1)
+print(armat2)
+
+
